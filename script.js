5424e9c9fdb489176b22c20adf0d6968e5d9dddc
Muneer Girnari <muneeragirnari@gmail.com>
2023-07-27 01:08:19 +0530

    solved js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
-    return sampleObject[key];
+  return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+
